c726c1b1425bc5d16b4e93fd3fae85d5aadd91e8
Giovani Rocha Lima <Watzeckdev@gmail.com>
2022-08-23 20:34:22 -0300

    Exercicio 1 e 2 OO

---
+++ Orientacao a Objetos/Exercicio001/Exercicio001/Entidades/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicio1.Entidades
+{
+    internal class Pessoa
+    {
+        internal string Nome { get; set; }
+        internal DateTime Nascimento { get; set; }
+        internal long CPF { get; set; }
+        internal long Telefone { get; set; }
+
+        public Pessoa()
+        {
+        }
+
+        internal Pessoa(string nome, DateTime nascimento, long cpf, long telefone)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            CPF = cpf;
+            Telefone = telefone;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}\nData de Nascimento: {Nascimento}\nCPF: {CPF}\nTelefone: {Telefone}";
+        }
+
+
+    }
+}
+
---
+++ Orientacao a Objetos/Exercicio001/Exercicio001/Program.cs
@@ -0,0 +1,45 @@
+﻿//CRIE UM PROGRAMA QUE CRIE 3 OBJETOS VINDO DE UMA CLASSE PESSOA QUE TEM QUATRO ATRIBUTOS,
+//NOME,
+//DATA DE NASCIMENTO
+//CPF
+//TELEFONE
+
+using System;
+using Exercicio1.Entidades;
+using System.Globalization;
+using Microsoft.VisualBasic;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] p = new Pessoa[3];
+
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                p[i] = new Pessoa();
+
+                Console.Write($"Informe o nome da {i + 1}º pessoa: ");
+                p[i].Nome = Console.ReadLine();
+
+                Console.Write($"Informe a data de nascimento da {i + 1}º pessoa: ");
+                p[i].Nascimento = DateTime.Parse(Console.ReadLine());
+
+                Console.Write($"Informe o CPF da {i + 1}º pessoa: ");
+                p[i].CPF = long.Parse(Console.ReadLine());
+
+                Console.Write($"Informe o telefone da {i + 1}º pessoa: ");
+                p[i].Telefone = long.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.WriteLine(p[i].ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
